c74b64f0c917b2e3738a25b7e60e92656b0242bc
pachikuriii <stakahashi0728@gmail.com>
2022-11-28 21:26:35 +0900

    管轄が複数にまたがる場合のパターンを追加

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,10 @@
 const { Oaza } = require("jp-zipcode-lookup");
 const zip7Data = require("../data/zip7.json");
 const cityData = require("../data/city.json");
-const oaza = Oaza.byZipcode("6511321")[0];
+const bothData = require("../data/both.json");
+const oaza = Oaza.byZipcode("0140205")[0];
 const zip7HelloWorks = Object.keys(zip7Data["zip7"]);
+const bothHelloWorks = Object.keys(bothData["both"]);
 const cityHelloWorks = Object.keys(cityData["city"]);
 const yourHelloWork = [];
 for (const zip7HelloWork of zip7HelloWorks) {
@@ -12,6 +14,11 @@ for (const zip7HelloWork of zip7HelloWorks) {
     }
 }
 if (yourHelloWork.length === 0) {
+    for (const bothHelloWork of bothHelloWorks) {
+        if (oaza.code in bothData["both"][bothHelloWork]) {
+            yourHelloWork.push(bothHelloWork);
+        }
+    }
     for (const cityHelloWork of cityHelloWorks) {
         if (oaza.city.code in cityData["city"][cityHelloWork]) {
             yourHelloWork.push(cityHelloWork);
